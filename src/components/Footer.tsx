3391babea55bffac78bedad83cd58089fd179a3a
Matan <matan3sh@gmail.com>
2024-03-19 21:12:13 +0200

    Footer Styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,18 @@
 import WordMark from "@/components/WordMark";
 import { createClient } from "@/prismicio";
 import { PrismicNextLink } from "@prismicio/next";
+import Link from "next/link";
 
 export default async function Footer() {
   const client = createClient();
   const settings = await client.getSingle("settings");
 
   return (
-    <footer>
-      <WordMark />
+    <footer className="flex flex-col items-center justify-between gap-6 border-t border-slate-600 px-8 py-7 md:flex-row">
+      <Link href={"/"}>
+        <WordMark />
+        <span className="sr-only">Glisten.ai Home Page</span>
+      </Link>
       <nav>
         <ul>
           {settings.data.navigation.map((item) => (
